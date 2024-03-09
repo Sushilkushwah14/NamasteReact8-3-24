89a9118b36c8a64c5f5611fd532df81b0e676950
Sushilkushwah14 <sushilkushwah1411@gmail.com>
2024-03-09 17:46:22 +0530

    added package.json and updated files

--- App.js
+++ App.js
@@ -1,6 +1,11 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+
+
 const parent=React.createElement("div",{id:parent},[
     ,React.createElement("div",{id:"child"},[
-        React.createElement("h1",{},"i am h1 tag"),
+        React.createElement("h1",{},"this is namaste react!"),
         React.createElement("h2",{},"i am h2 tag")
     ]),
     React.createElement("div",{id:"child2"},[
