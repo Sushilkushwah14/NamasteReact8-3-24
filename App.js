7e4216cd12ef366af4cb47f3ec91f661e4a9e925
Sushilkushwah14 <sushilkushwah1411@gmail.com>
2024-03-11 20:43:47 +0530

    correct the App code

--- App.js
+++ App.js
@@ -3,10 +3,10 @@ import ReactDOM from "react-dom/client";
 
 
 
-const parent=React.createElement("div",{id:parent},[
-    ,React.createElement("div",{id:"child"},[
+const parent=React.createElement("div",{id:"parent"},[
+    React.createElement("div",{id:"child"},[
         React.createElement("h1",{},"this is namaste react!"),
-        React.createElement("h2",{},"i am h2 tag")
+        React.createElement("h2",{},"i am h2 tag hii sushil")
     ]),
     React.createElement("div",{id:"child2"},[
         React.createElement("h1",{},"i am h1 tag"),
@@ -15,6 +15,6 @@ const parent=React.createElement("div",{id:parent},[
 
 ]);
 
-console.log(parent);
+
 ReactDOM.createRoot(document.getElementById("root"))
 .render(parent)
